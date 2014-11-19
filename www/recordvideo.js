38fcdf1c6155d5a75d91beaa7f9e59e892ae37bd
Ambikag13 <ambikag@tataelxsi.co.in>
2014-11-20 00:48:53 +0530

    Changes further

--- www/recordvideo.js
+++ www/recordvideo.js
@@ -1,5 +1,5 @@
 var record = {
-    createEvent: function(successCallback, errorCallback) {
+    recordVideo: function(successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
