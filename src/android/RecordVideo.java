02980f252bb97f5217fce59861ceee254ec54762
unknown <ambikag@ambikag.telxsi.com>
2014-11-21 10:07:33 +0530

    Changes to the plugin to on activity result

--- src/android/RecordVideo.java
+++ src/android/RecordVideo.java
@@ -18,21 +18,41 @@
 */
 package com.snipme.record;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginManager;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.file.FileUtils;
+import org.apache.cordova.file.LocalFilesystemURL;
+import org.apache.cordova.mediacapture.Capture;
+import org.apache.cordova.mediacapture.FileHelper;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
-public class RecordVideo extends CordovaPlugin {
+public class Record extends CordovaPlugin {
 
+	private static final String TAG = Record.class.getName();
 
-	public static final String RECORD_VIDEO = "recordVideo"; 
+	public static final String RECORD_VIDEO = "recordVideo";
+	private static final int CAPTURE_VIDEO = 0;
+	private static final int CAPTURE_NO_MEDIA_FILES = 3;
 
-	private static final String TAG = RecordVideo.class.getName();
+	private static final String VIDEO_3GPP = "video/3gpp";
+	private static final String VIDEO_MP4 = "video/mp4";
+	private static final String AUDIO_3GPP = "audio/3gpp";
+	private static final String IMAGE_JPEG = "image/jpeg";
 
     private CallbackContext callbackContext;        // The callback context from which we were invoked.
     private JSONArray results;                      // The array of results to be returned to the user
@@ -49,39 +69,11 @@ public class RecordVideo extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-       /* this.callbackContext = callbackContext;
-        //this.limit = 1;
-        //this.duration = 0;
-        this.results = new JSONArray();
-
-        JSONObject options = args.optJSONObject(0);
-        if (options != null) {
-            limit = options.optLong("limit", 1);
-            duration = options.optInt("duration", 0);
-        }
-
-        if (action.equals("getFormatData")) {
-            JSONObject obj = getFormatData(args.getString(0), args.getString(1));
-            callbackContext.success(obj);
-            return true;
-        }
-        else if (action.equals("captureAudio")) {
-            this.captureAudio();
-        }
-        else if (action.equals("captureImage")) {
-            this.captureImage();
-        }
-        else if (action.equals("captureVideo")) {
-            this.captureVideo(duration);
-        }
-        else {
-            return false;
-        }*/
-    	Log.i(TAG, "execute method");
+    	Log.i(TAG, "execute method  "+action);
     	try{
     		if(RECORD_VIDEO.equals("recordVideo")){
         		Intent intent = new Intent(this.cordova.getActivity(), CameraActivity.class);
-        		this.cordova.getActivity().startActivity(intent);
+        		this.cordova.getActivity().startActivityForResult(intent, CAPTURE_VIDEO);
         		callbackContext.success();
         		return true;
         	}
@@ -92,19 +84,140 @@ public class RecordVideo extends CordovaPlugin {
     	    callbackContext.error(e.getMessage());
     	    return false;
     	} 
+    }
+    
+	private String getTempDirectoryPath() {
+        File cache = null;
+
+        // Use internal storage
+        cache = cordova.getActivity().getCacheDir();
+
+        // Create the cache directory if it doesn't exist
+        cache.mkdirs();
+        return cache.getAbsolutePath();
+    }
+    
+    /**
+     * Creates a JSONObject that represents a File from the Uri
+     *
+     * @param data the Uri of the audio/image/video
+     * @return a JSONObject that represents a File
+     * @throws IOException
+     */
+    private JSONObject createMediaFile(Uri data) {
+        File fp = webView.getResourceApi().mapUriToFile(data);
+        JSONObject obj = new JSONObject();
+
+        Class webViewClass = webView.getClass();
+        PluginManager pm = null;
+        try {
+            Method gpm = webViewClass.getMethod("getPluginManager");
+            pm = (PluginManager) gpm.invoke(webView);
+        } catch (NoSuchMethodException e) {
+        } catch (IllegalAccessException e) {
+        } catch (InvocationTargetException e) {
+        }
+        if (pm == null) {
+            try {
+                Field pmf = webViewClass.getField("pluginManager");
+                pm = (PluginManager)pmf.get(webView);
+            } catch (NoSuchFieldException e) {
+            } catch (IllegalAccessException e) {
+            }
+        }
+        FileUtils filePlugin = (FileUtils) pm.getPlugin("File");
+        LocalFilesystemURL url = filePlugin.filesystemURLforLocalPath(fp.getAbsolutePath());
+
+        try {
+            // File properties
+            obj.put("name", fp.getName());
+            obj.put("fullPath", fp.toURI().toString());
+            if (url != null) {
+                obj.put("localURL", url.toString());
+            }
+            // Because of an issue with MimeTypeMap.getMimeTypeFromExtension() all .3gpp files
+            // are reported as video/3gpp. I'm doing this hacky check of the URI to see if it
+            // is stored in the audio or video content store.
+            if (fp.getAbsoluteFile().toString().endsWith(".3gp") || fp.getAbsoluteFile().toString().endsWith(".3gpp")) {
+                if (data.toString().contains("/audio/")) {
+                    obj.put("type", AUDIO_3GPP);
+                } else {
+                    obj.put("type", VIDEO_3GPP);
+                }
+            } else {
+                obj.put("type", FileHelper.getMimeType(Uri.fromFile(fp), cordova));
+            }
+
+            obj.put("lastModifiedDate", fp.lastModified());
+            obj.put("size", fp.length());
+        } catch (JSONException e) {
+            // this will never happen
+            e.printStackTrace();
+        }
+        return obj;
+    }
+    
+    /**
+     * Called when the video view exits.
+     *
+     * @param requestCode       The request code originally supplied to startActivityForResult(),
+     *                          allowing you to identify who this result came from.
+     * @param resultCode        The integer result code returned by the child activity through its setResult().
+     * @param intent            An Intent, which can return result data to the caller (various data can be attached to Intent "extras").
+     * @throws JSONException
+     */
+    public void onActivityResult(int requestCode, int resultCode, final Intent intent) {
+    	 if (resultCode == Activity.RESULT_OK) {
+    		 if (requestCode == CAPTURE_VIDEO) {
+    			    Log.i(TAG, "onActivityResult: Video Capture success");
+    	            final Record that = this;
+    	            Runnable captureVideo = new Runnable() {
+
+    	                @Override
+    	                public void run() {
+    	                
+    	                    Uri data = null;
+    	                    
+    	                    if (intent != null){
+    	                        // Get the uri of the video clip
+    	                        data = intent.getData();
+    	                        Log.i(TAG, "onActivityResult: Uri: "+data);
+    	                        Log.i(TAG, "Uri of the video file recorded");
+    	                    }
+    	                    
+    	                    if( data == null){
+    	                       File movie = new File(getTempDirectoryPath(), "Capture.avi");
+    	                       data = Uri.fromFile(movie);
+    	                    }
+    	                    
+    	                    // create a file object from the uri
+    	                    if(data == null)
+    	                    {
+    	                        that.fail(createErrorObject(CAPTURE_NO_MEDIA_FILES, "Error: data is null"));
+    	                    }
+    	                    else
+    	                    {
+    	                        results.put(createMediaFile(data));
+
+    	                        if (results.length() >= 1) {
+    	                            // Send Uri back to JavaScript for viewing video
+    	                            that.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, results));
+    	                        } else {
+    	                            // still need to capture more video clips
+    	                            //captureVideo(duration);
+    	                        }
+    	                    }
+    	                }
+    	            };
+    	            this.cordova.getThreadPool().execute(captureVideo);
+    	        }
+    	 }else if (resultCode == Activity.RESULT_CANCELED) {
+    		 
+    	 }
     	
     }
 
    
-   
-    /**
-     * Sets up an intent to capture video.  Result handled by onActivityResult()
-     */
-    private void recordVideo() {
-    	Intent intent = new Intent(this.cordova.getActivity(), CameraActivity.class);
-		this.cordova.getActivity().startActivity(intent);
-		callbackContext.success();
-    }
 
    
 
