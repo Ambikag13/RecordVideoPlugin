d24745de75ba11269951829dbe742bfc4f4d21fb
unknown <ambikag@ambikag.telxsi.com>
2015-01-02 17:45:48 +0530

    Added options support in java script file.

--- www/recordvideo.js
+++ www/recordvideo.js
@@ -1,12 +1,23 @@
 var record = {
-    recordVideo: function(successCallback, errorCallback) {
+    recordVideo: function(successCallback, errorCallback, options) {
+    	var path = null;
+        var assets = null;
+        var quote = null;
+        var cutin = null
+        
+        if (options) {
+        	path = options.path;
+        	assets = options.assets;
+        	quote = options.quote;
+        	cutin = options.cutin;
+        }
+        
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'RecordVideo', // mapped to our native Java class called "CalendarPlugin"
+            'RecordVideo', // mapped to our native Java class called "RecordVideo"
             'recordVideo', // with this action name
-            [{                  // and this array of custom arguments to create our entry
-            }]
+            [options]
         ); 
     }
 }
